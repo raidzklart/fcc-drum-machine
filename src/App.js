c244bb4866db39ddd79469fb77eb75eadb47d4b7
Thomas Christopher <thomas.j.christopher@gmail.com>
2019-07-06 23:09:45 +0100

    Initial commit and initial layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  padClicked(id) {
+    console.log(id);
+  }
+  render() {
+    return (
+      <div id="drum-machine" className="main">
+        <div id="display">
+          <h1>Hello</h1>
+        </div>
+        <div className="pads">
+          <button id="Q" className="drum-pad" onClick={() => { this.padClicked('Q') }}>Q</button>
+          <button id="W" className="drum-pad" onClick={() => { this.padClicked('W') }}>W</button>
+          <button id="E" className="drum-pad" onClick={() => { this.padClicked('E') }}>E</button>
+          <button id="A" className="drum-pad" onClick={() => { this.padClicked('A') }}>A</button>
+          <button id="S" className="drum-pad" onClick={() => { this.padClicked('S') }}>S</button>
+          <button id="D" className="drum-pad" onClick={() => { this.padClicked('D') }}>D</button>
+          <button id="Z" className="drum-pad" onClick={() => { this.padClicked('Z') }}>Z</button>
+          <button id="X" className="drum-pad" onClick={() => { this.padClicked('X') }}>X</button>
+          <button id="C" className="drum-pad" onClick={() => { this.padClicked('C') }}>C</button>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+
